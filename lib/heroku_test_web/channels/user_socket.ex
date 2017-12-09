b776dd9d9f776cc813db93aab312d9ee38a7bd6f
Łukasz Skotarek <lukasz.skotarek@dgc.se>
2017-12-09 13:53:38 +0100

    some heroku stuff

--- lib/heroku_test_web/channels/user_socket.ex
+++ lib/heroku_test_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule HerokuTestWeb.UserSocket do
   # channel "room:*", HerokuTestWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
